ba0e0ce6bc02adea7244c5a69419c5b144575191
Erick Pacheco <58119210+erick-pacheco@users.noreply.github.com>
2022-11-21 15:56:03 +0000

    Refactor and make all tests pass

---
+++ Cat.java
@@ -0,0 +1,5 @@
+public class Cat extends Animal {
+  public void meow(){
+    System.out.println(this.name + " the cat meows loudly.");
+  }
+}
--- Main.java
+++ Main.java
@@ -3,7 +3,32 @@ public class Main {
 
   public static void main(String[] args) {
 
+    /* Upcast Dog object to Animal */
+    Animal pet = new Dog();
+    pet.name = "Charlie";
+    pet.doTrick();
+
+    /* Section 5 */
+    Animal Wanda = new Cat();
+    Wanda.name = "Wanda";
+    Wanda.doTrick();
+
+    /* Section 7 */
+    makeAnimalSpeak(pet);
+
+    /* Section 9 */
+    makeAnimalSpeak(Wanda);
+  }
+
+  /* Section 6 */
+  public static void makeAnimalSpeak(Animal animal) {
+    if (animal instanceof Dog) {
+      Dog dog = (Dog) animal;
+      dog.bark();
+    } else if (animal instanceof Cat) { /* Section 8 */
+      Cat cat = (Cat) animal;
+      cat.meow();
+    }
   }
 
 }
-
